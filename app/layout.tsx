35a27fe833d78336bc74a15cfaf6d5f9b4117673
lucas <e12323781@student.tuwien.ac.at>
2025-07-30 15:26:33 +0200

    player mock added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="de">
+        <html lang="de" className="bg-primary">
         <body className="font-sans bg-white text-black flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-1">{children}</main>
