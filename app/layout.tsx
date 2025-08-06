779ffac46fb38655c51fa0a3361db1bc8cda84bb
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-08-06 10:14:09 +0200

    Change font, start with landingpage

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,24 +2,31 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Head from "next/head";
-import { Inter } from "next/font/google";
+import { Geist } from "next/font/google";
+import { Metadata } from "next";
 
-const font = Inter({
+const font = Geist({
   subsets: ["latin"],
   weight: ["400", "700"],
 });
+
+export const metadata: Metadata = {
+  title: "Sportfreunde Nofels",
+  description:
+    "Hier findest du alles über unseren Verein, die Hobbyliga, unsere Mannschaft und den legendären BeachsoccerCup!",
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="de" className="bg-primary">
+    return (
+      <html lang="de" className="bg-primary">
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body  className={font.className}>
+      <body className={font.className}>
         <Navbar />
 
         <main className="flex-1">{children}</main>
