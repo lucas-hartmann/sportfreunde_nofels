814d6d64a4e2c1f7f978a7fa77d63958d245e85f
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-08-06 09:44:56 +0200

    Set custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,30 @@
-import './globals.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Head from "next/head";
+import { Inter } from "next/font/google";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang="de" className="bg-primary">
-        <Head>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <body className="font-sans bg-white text-black flex flex-col min-h-screen">
+const font = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="de" className="bg-primary">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body  className={font.className}>
         <Navbar />
 
         <main className="flex-1">{children}</main>
         <Footer />
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
