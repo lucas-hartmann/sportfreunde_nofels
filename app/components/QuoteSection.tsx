9cf4065c3faaf15f5699b63457956272172a4bf9
Hiebeler <emanuel.hiebeler@gmail.com>
2025-09-11 18:49:43 +0200

    add image to zitat

--- app/components/QuoteSection.tsx
+++ app/components/QuoteSection.tsx
@@ -2,18 +2,19 @@ import Image from "next/image";
 
 export default function QuoteSection() {
   return (
-    <section className="py-20 bg-gray-200 flex items-center justify-center">  
+    <section className="py-20 bg-gray-200 flex items-center justify-center">
       <div className="max-w-4xl mx-auto px-6 md:px-10">
         <blockquote className="text-2xl md:text-3xl font-light text-gray-500 text-center">
-          "Bei einem Fußballspiel verkompliziert sich alles durch die Anwesenheit der gegnerischen Kantine."
+          "Bei einem Fußballspiel verkompliziert sich alles durch die
+          Anwesenheit der gegnerischen Kantine."
         </blockquote>
 
         <div className="flex items-center justify-center space-x-5 mt-8">
           <Image
-            src="/logos/sfn_logo.webp"
-            width={60}
-            height={60}
-            className="w-15 h-15 rounded-full"
+            src="/headers/zitat_cropped.webp"
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full"
             alt=""
           />
 
