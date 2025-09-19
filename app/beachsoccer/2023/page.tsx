7b506be6dd8836c159eaf99bc2744b9fe95f42fe
lucas <e12323781@student.tuwien.ac.at>
2025-09-19 13:25:47 +0300

    add bsc23

--- app/beachsoccer/2023/page.tsx
+++ app/beachsoccer/2023/page.tsx
@@ -1,13 +1,17 @@
 import Header from "@/app/components/Header";
+import ImageGallery from "@/app/components/ImageGallery";
 
 export default function Gallery2023(){
     return(
         <span>
         {/* Header */}
-            <Header
-                title="Beachsoccer Cup 2023"
-                image="/headers/bsc23.webp"
-            />
+        <Header
+            title="Beachsoccer Cup 2023"
+            image="/headers/bsc23.webp"
+        />
+        <main className="min-h-screen bg-neutral-50 py-12 px-6">
+            <ImageGallery folder="beachsoccer2023" />
+        </main>
         </span>
     );
 }
